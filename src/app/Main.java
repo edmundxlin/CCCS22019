8a94b954429a68fb6330aff98f0de03531a79d39
edmundxlin <edmundxlin@users.noreply.github.com>
2019-12-20 20:19:52 -0500

    CCC 2019 S2 Solution
    Solution to CCC 2019 S2

---
+++ src/app/Main.java
@@ -0,0 +1,43 @@
+package app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    static boolean isPrime(long n) {
+        if (n < 2)
+            return false;
+        if (n == 2 || n == 3)
+            return true;
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+        long sqrtN = (long) Math.sqrt(n) + 1;
+        for (long i = 6L; i <= sqrtN; i += 6) {
+            if (n % (i - 1) == 0 || n % (i + 1) == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i<T; i++){
+            int N = Integer.parseInt(br.readLine());
+            for(int A = N, B = N; A>=2; A--, B++){
+                if(isPrime(A)&&isPrime(B)){
+                    System.out.println(A + " "+ B);
+                    break;
+                }
+            }
+        }
+            
+    }
+}
